4addd8f166212ff718df35c2d3456d0df86110d9
Lantianyou <dtlantianyou@gmail.com>
2020-08-29 21:25:38 +0800

    k8s

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,16 +7,23 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const events = [];
 app.post("/events", (req, res) => {
   const event = req.body;
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-  axios.post("http://localhost:4003/events", event);
+  events.push(event);
+
+  axios.post("http://posts-clusterip-srv:4000/events", event);
+  // axios.post("http://localhost:4001/events", event);
+  // axios.post("http://localhost:4002/events", event);
+  // axios.post("http://localhost:4003/events", event);
 
   res.send({ status: "ok" });
 });
 
 app.listen(4005, () => {
   console.log(`listening in ${4005}`);
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
