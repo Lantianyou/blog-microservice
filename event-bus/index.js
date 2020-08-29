61cd24b9da638d9c5f261804416d06b99bbf5289
Lantianyou <dtlantianyou@gmail.com>
2020-08-29 09:59:58 +0800

    all micro services

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
   const event = req.body;
-  axios.post('http://localhost:4000/events', event)
-  axios.post('http://localhost:4001/events', event)
-  axios.post('http://localhost:4002/events', event)
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+  axios.post("http://localhost:4003/events", event);
 
-  res.send({status:'ok'})
-})
+  res.send({ status: "ok" });
+});
 
 app.listen(4005, () => {
-  console.log(`listening in ${4005}`)
-})
+  console.log(`listening in ${4005}`);
+});
--- query/index.js
+++ query/index.js
@@ -1,19 +1,43 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/post', (req, res) => {
-  
-})
+const posts = {};
 
-app.post('/events', (req, res) => {
-  
-})
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === "CommentUpdated") {
+    const { postId, id, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id === id);
+
+    comment.status = status;
+    comment.content = content;
+  }
+
+  res.send({});
+});
 
 app.listen(4002, () => {
-  console.log('listening in 4002')
-})
+  console.log("listening in 4002");
+});
